162a4ceb8012621ef2cd651520d7c4ca2ae7da67
Pedro Prata <pedropratareis@gmail.com>
2023-07-03 01:36:37 -0300

    corrige um erro

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let energia = Number(document.querySelector("#energia").children[0].innerHTML);
 const pontos_jogador = document.querySelectorAll(".pontos_jogador");
 
 let arrastado = null;
+let origemCarta = null;
 
 for (let i = 0; i < eCarta.length; i++) {
     eCarta[i].addEventListener('dragstart', comecaArrastar);
@@ -16,6 +17,7 @@ for (let i = 0; i < eSlot.length; i++) {
 
 function comecaArrastar (evento){
     arrastado=evento.target;
+    origemCarta=evento.target.parentElement;
 }
 
 function passouPorCima (evento){
@@ -23,30 +25,29 @@ function passouPorCima (evento){
 }
 
 function recebeAlgo (evento){
-    const poder_carta = Number(arrastado.children[1].innerHTML);
-    const energia_carta = Number(arrastado.children[0].innerHTML);
 
     if(evento.target.classList.contains("ocupado") || evento.target.classList.contains("pc")){
         return;
     }
     if(evento.target.classList.contains("slot_carta")){
+        const poder_carta = Number(arrastado.children[1].innerHTML);
+        const energia_carta = Number(arrastado.children[0].innerHTML);
         if(energia >= energia_carta){
             const campo = Number(evento.target.parentElement.parentElement.getAttribute('value'));
             let pontos = Number(pontos_jogador[campo].innerHTML);
             pontos_jogador[campo].innerHTML = pontos + poder_carta;
             energia = energia - energia_carta;
             document.querySelector("#energia").children[0].innerHTML = energia;
-            arrastado.setAttribute('draggable', 'false');
 
             evento.target.classList.add("ocupado");
             origemCarta.classList.remove("ocupado");
             origemCarta = null;
+
             evento.target.appendChild(arrastado);
+            arrastado.setAttribute('draggable', 'false');
             arrastado = null;
         }else{
             alert("Sem energia!");
         }
     }
-    
-    arrastado = null;
 }
